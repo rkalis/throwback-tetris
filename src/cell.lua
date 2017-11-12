8f098607ed08f41d946e2eb36f536c6676a664b2
Rosco Kalis <roscokalis@gmail.com>
2017-11-12 11:01:21 +0100

    Added debug printing to all cells

--- src/cell.lua
+++ src/cell.lua
@@ -5,7 +5,7 @@ function Cell:new(x, y, size)
     local obj = {
         x = x,
         y = y,
-        size = size,
+        size = size
     }
     setmetatable(obj, self)
     self.__index = self
@@ -20,7 +20,7 @@ function Cell:drawSprite(sprite)
     love.graphics.draw(sprite, self.x, self.y, 0, self.size / 120)
 end
 
-function Cell:draw(colour)
+function Cell:draw(colour, x, y)
     if colour then
         love.graphics.setColor(colour)
         love.graphics.rectangle("fill", self.x, self.y, self.size, self.size)
@@ -28,6 +28,9 @@ function Cell:draw(colour)
 
     love.graphics.setColor(100,100,100)
     love.graphics.rectangle("line", self.x, self.y, self.size, self.size)
+    if x and y then
+        love.graphics.print(x .. ',' .. y, self.x, self.y)
+    end
     love.graphics.setColor(255,255,255)
 end
 
